c3c9aaac1d68ff5f95a52424a377452c96d1bbd1
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-03 06:38:11 +0000

    Updated attributes to accessor; received an error - need to change to accessor to manually create characters

--- character.rb
+++ character.rb
@@ -1,5 +1,5 @@
 class Character
-  attr_reader :name
+  attr_accessor :name
   attr_accessor :health_hp
 
   def initialize
@@ -9,8 +9,8 @@ class Character
 end
 
 class Hero < Character
-  attr_reader :wand_power
-  attr_reader :special_power
+  attr_accessor :wand_power
+  attr_accessor :special_power
 
   def initialize
     @wand_power = wand_power
@@ -25,3 +25,23 @@ class Villain < Character
     @evil_power = evil_power
   end
 end
+
+wizard1 = Hero.new
+wizard1.name = "Harry Potter"
+wizard1.wand_power = 17
+
+wizard2 = Hero.new
+wizard2.name = "Hermione Granger"
+wizard2.wand_power = 20
+
+wizard3 = Hero.new
+wizard3.name = "Ron Weasley"
+wizard3.wand_power = 16
+
+wizard4 = Hero.new
+wizard4.name = "Neville Longbottom"
+wizard4.wand_power = 14
+
+pp wizard1.name
+pp wizard1.wand_power
+pp wizard1.health_hp
