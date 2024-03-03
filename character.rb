c37b82ac59c4de01b953cd2b78662afefb684a45
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-03 23:27:35 +0000

    Created villain characters

--- character.rb
+++ character.rb
@@ -1,16 +1,21 @@
 class Character
   attr_accessor :name, :health_hp, :role
 
-  def initialize(name, health_hp, role) 
+  def initialize(name, health_hp, role, attack_power) 
     @name = name
     @health_hp = health_hp
     @role = role
+    @attack_power = attack_power
   end
 end
 
-wizard1 = Character.new("Harry Potter", 100, "Howgarts Wizard")
-wizard2 = Character.new("Hermione Granger", 100, "Hogwarts Wizard")
-wizard3 = Character.new("Ron Weasley", 100, "Hogwarts Wizard")
-wizard4 = Character.new("Severus Snape", 100, "Howgarts Teacher")
+wizard1 = Character.new("Harry Potter", 100, "Howgarts Wizard", 17)
+wizard2 = Character.new("Hermione Granger", 100, "Hogwarts Wizard", 20)
+wizard3 = Character.new("Ron Weasley", 100, "Hogwarts Wizard", 16)
+wizard4 = Character.new("Severus Snape", 100, "Howgarts Teacher", 40)
 
-pp wizard1
+villain1 = Character.new("Basilisk", 100, "Large Snake", 50)
+villain2 = Character.new("Bellatrix LeStrange", 100, "Death Eater", 36)
+villain3 = Character.new("Aragog", 100, "Acromantula", 60)
+villain4 = Character.new("Lupin", 100, "Werewolf", 45)
+
