0ff54c6f0e31a4cd339a30b191851e5512c7e4ef
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-03 23:56:45 +0000

    Update

--- quest.rb
+++ quest.rb
@@ -2,6 +2,6 @@ require_relative "character"
 
 class Quest
   def choose_character
-    "This is the HP Quest! You need to choose a character"
+    return "This is the HP Quest! You need to choose a character"
   end
 end
