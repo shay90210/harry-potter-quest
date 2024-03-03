56336aecd94983983e7ce0f9abc2c7647402796d
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-03 06:09:26 +0000

    added attritbutes to class Character and inheritors

--- character.rb
+++ character.rb
@@ -1,10 +1,17 @@
 class Character
+  attr_accessor :name
+  attr_accessor :health_hp
+
   def initialize
+    :name = name
+    :health_hp = 100
   end
 end
 
 class Hero < Character
+  attr_accessor :wand_power
 end
 
 class Villain < Character
+  attr_accessor :power
 end
