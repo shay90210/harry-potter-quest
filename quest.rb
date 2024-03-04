2eb0e181c2b26004509d1118579212b34a73b86c
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-04 02:40:46 +0000

    add puts statements and began gets.chomp input

--- quest.rb
+++ quest.rb
@@ -2,6 +2,13 @@ require_relative "character"
 
 class Quest
   def choose_character
-    return "This is the HP Quest! You need to choose a character"
+    puts "Welcome to the wizarding world of Harry Potter. You will need to journey on a quest to get out of a dire situation!"
+    puts "Choose your character: Be wise as you may not know how your journey will end..."
+    puts "1. Harry Potter"
+    puts "2. Hermione Granger"
+    puts "3. Ron Weasley"
+    puts "4. Severus Snape"
+
+    choice = gets.chomp
   end
 end
