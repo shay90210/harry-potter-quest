5202decf8a4d2111d91908d9fc2799c369cb7686
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-05 01:15:57 +0000

    added gets.chomp and more puts statements

--- quest.rb
+++ quest.rb
@@ -43,14 +43,21 @@ class Quest
   def potter_quest
     puts "Join Harry Potter in the Chamber of Secrets as he is searching for Ginny Weasley. She was kidnapped by the heir of Slytherin and needs to be returned to safety at Hogwarts!"
     puts "Would you like to continue?"
+
     option = gets.chomp
     puts "Y"
     puts "N"
-    if option == n.upcase.downcase
-      break
+    if option == "N" || "n"
       puts "We will save Ginny next time..."
+    else 
+      puts "Let's proceed..."
     end
 
+    puts "Harry enters a hall covered in snakes and hears a hisssssssssssssssssssss...."
+    puts "What should Harry do?"
+    option = gets.chomp
+    puts "1. Check it out."
+    puts "2. Wait to see what happens."
   end
 
   def granger_quest
