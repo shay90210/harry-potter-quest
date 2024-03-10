31a1dc34ef0ee939a0b2be49a413c8746fbdb3da
Shannon Bentley <bentley.shannon@yahoo.com>
2024-03-10 00:50:15 +0000

    removed and added a line break

--- quest.rb
+++ quest.rb
@@ -57,9 +57,9 @@ class CharacterQuest
     puts "Harry is looking for Ginny in the Chamber of Secrets. The heir of Slytherin kidnapped her and threatened to harm her!"
     puts "\n"
     puts "Harry enters a tunnel and hears a hisssssssssssssssssssssssssssssss...."
-    puts "What should Harry do?"
     puts "\n"
-
+    
+    puts "What should Harry do?"
     puts "1. Check it out."
     puts "2. Wait to see what happens."
 
