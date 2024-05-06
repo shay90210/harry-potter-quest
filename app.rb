d8638761b30e359a8dd1e8a460d363c5397eed6a
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-06 22:03:21 +0000

    added weasley class to the app.rb

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require_relative "quest"
 require_relative "potter"
 require_relative "granger"
+require_relative "weasley"
 require_relative "player"
 
 quest = Quest.new
 
 potter = Potter.new
 granger = Granger.new
+weasley = Weasley.new
 
 player = Player.new
 
-quest.choose_character(potter, granger, player)
+quest.choose_character(potter, granger, weasley, player)
