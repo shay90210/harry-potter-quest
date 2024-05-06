a9cb1e3006fb9532c47657e802b421bec8a264f1
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-06 21:47:46 +0000

    added player class to app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require_relative "quest"
 require_relative "potter"
+require_relative "player"
 
 quest = Quest.new
 potter = Potter.new
+player = Player.new
 
-quest.choose_character(potter)
+quest.choose_character(potter, player)
