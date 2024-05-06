736f9ecfd9962f99873b32908fd1a22df57e8370
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-06 22:00:23 +0000

    added granger class to app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require_relative "quest"
 require_relative "potter"
+require_relative "granger"
 require_relative "player"
 
 quest = Quest.new
+
 potter = Potter.new
+granger = Granger.new
+
 player = Player.new
 
-quest.choose_character(potter, player)
+quest.choose_character(potter, granger, player)
