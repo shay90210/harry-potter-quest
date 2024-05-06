ac058a0d0f7137b50eeb96e14435c97cb6cdde8f
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-06 21:36:45 +0000

    pasted the def potter_quest into the Potter class

--- potter_quest.rb
+++ potter_quest.rb
@@ -1,4 +1,21 @@
 class Potter
   def potter_quest
+    puts "\n"
+    puts "Harry is looking for Ginny in the Chamber of Secrets. The heir of Slytherin kidnapped her and threatened to harm her!"
+    puts "\n"
+    puts "Harry enters a tunnel and hears a hisssssssssssssssssssssssssssssss...."
+    puts "\n"
+
+    puts "What should Harry do?"
+    puts "1. Check it out."
+    puts "2. Wait to see what happens."
+
+    option = gets.chomp.to_i
+    case option
+    when 1
+      basilisk_battle
+    else
+      puts "The sound is getting closer and more vivid..."
+    end
   end
 end
--- quest.rb
+++ quest.rb
@@ -55,23 +55,7 @@ class Quest
 
   # EACH INDIVIDUAL CHARACTER QUEST - Beginning w/ Harry Potter
   def potter_quest
-    puts "\n"
-    puts "Harry is looking for Ginny in the Chamber of Secrets. The heir of Slytherin kidnapped her and threatened to harm her!"
-    puts "\n"
-    puts "Harry enters a tunnel and hears a hisssssssssssssssssssssssssssssss...."
-    puts "\n"
 
-    puts "What should Harry do?"
-    puts "1. Check it out."
-    puts "2. Wait to see what happens."
-
-    option = gets.chomp.to_i
-    case option
-    when 1
-      basilisk_battle
-    else
-      puts "The sound is getting closer and more vivid..."
-    end
   end
 
   # Hermione Granger's quest
