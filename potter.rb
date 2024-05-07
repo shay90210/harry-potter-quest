1391a1874c4443df2dc99f15b82219b359343061
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-07 04:58:14 +0000

    removed spells hash and methods from potter.rb

--- potter.rb
+++ potter.rb
@@ -27,11 +27,6 @@ class Potter
       "The basilisk gazed into Harry's eyes..."
     ]
 
-    cast_spell = {:wingardrium_leviosa => 8, :expulso => 25, :stupefy => 42, :petrifacus_totalus => 17, :incarcerus => 36, :lumos => 4}
-    cast_spell_array = cast_spell.to_a.sample
-    spell_key = cast_spell_array.at(0)
-    spell_value = cast_spell_array.at(1)
-
       puts "\n"
       puts "A basilisk appeared!"
       puts basilisk_attack.sample
