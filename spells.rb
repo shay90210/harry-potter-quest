10452d4d34919b550032c18a3782c20eb5dcfd08
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-07 05:26:23 +0000

    added part of a new spell object wingardrium leviosa

--- spells.rb
+++ spells.rb
@@ -8,6 +8,10 @@ class Spell
   end
 end
 
+wingardrium_leviosa = Spell.new(
+  name = "Wingardrium Leviosa"
+)
+
 Cast_spell = {:wingardrium_leviosa => 8, :expulso => 25, :stupefy => 42, :petrifacus_totalus => 17, :incarcerus => 36, :lumos => 4}
 cast_spell_array = cast_spell.to_a.sample
 spell_key = cast_spell_array.at(0)
