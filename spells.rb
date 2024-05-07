ba8de33b41cab4217f76425bb8616bb0209b5ac1
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-07 05:00:31 +0000

    added code removed from potter quest to reference and add to spell class

--- spells.rb
+++ spells.rb
@@ -4,3 +4,8 @@ class Spell
   def initialize
   end
 end
+
+Cast_spell = {:wingardrium_leviosa => 8, :expulso => 25, :stupefy => 42, :petrifacus_totalus => 17, :incarcerus => 36, :lumos => 4}
+cast_spell_array = cast_spell.to_a.sample
+spell_key = cast_spell_array.at(0)
+spell_value = cast_spell_array.at(1)
