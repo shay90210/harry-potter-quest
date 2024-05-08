3b6892a8dc6f5b2db88b18926ab2fc3c41168421
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-08 03:49:16 +0000

    switched the way the object values are listed to match each attribute name.

--- character.rb
+++ character.rb
@@ -25,10 +25,10 @@ class Villain < Character
   end
 end
 
-hero1 = Character.new("Harry Potter", 100, "Howgarts Wizard")
-hero2 = Character.new("Hermione Granger", 100, "Hogwarts Wizard")
-hero3 = Character.new("Ron Weasley", 100, "Hogwarts Wizard")
+hero1 = Character.new("Harry Potter", "Howgarts Wizard", 100)
+hero2 = Character.new("Hermione Granger", "Hogwarts Wizard", 100)
+hero3 = Character.new("Ron Weasley", "Hogwarts Wizard", 100)
 
-villain1 = Character.new("Basilisk", 100, "Large Snake", 50)
-villain2 = Character.new("Bellatrix LeStrange", 100, "Death Eater", 36)
-villain3 = Character.new("Aragog", 100, "Acromantula", 60)
+villain1 = Character.new("Basilisk", "Large Snake", 100, 50)
+villain2 = Character.new("Bellatrix LeStrange", "Death Eater", 100, 36)
+villain3 = Character.new("Aragog", "Acromantula", 100, 60)
