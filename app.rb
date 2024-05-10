707c2992ca8c0d9b507a8f58f9acfce5190ba299
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-10 13:46:37 +0000

    removed the basilisk require relative in app.rb for now

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require_relative "quest"
 require_relative "potter"
 require_relative "granger"
 require_relative "weasley"
-require_relative "basilisk"
 require_relative "player"
 
 quest = Quest.new
@@ -11,10 +10,6 @@ potter = Potter.new
 granger = Granger.new
 weasley = Weasley.new
 
-basilisk = Basilisk.new
-
 player = Player.new
 
 quest.choose_character(potter, granger, weasley, player)
-
-potter.potter_quest(basilisk)
