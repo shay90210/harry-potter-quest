919144ce001f68edd489040d7bfaecc7473cc7d6
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-10 13:34:30 +0000

    added require_relative to potter_quest for basilisk

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative "quest"
 require_relative "potter"
 require_relative "granger"
 require_relative "weasley"
+require_relative "basilisk"
 require_relative "player"
 
 quest = Quest.new
@@ -10,6 +11,10 @@ potter = Potter.new
 granger = Granger.new
 weasley = Weasley.new
 
+basilisk = Basilisk.new
+
 player = Player.new
 
 quest.choose_character(potter, granger, weasley, player)
+
+potter.potter_quest(basilisk)
