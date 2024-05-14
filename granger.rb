de5299f7553c7226629283c0c7e5111fd5bfac45
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-14 23:53:03 +0000

    updated puts statement for game

--- granger.rb
+++ granger.rb
@@ -1,6 +1,6 @@
 class Granger
   def granger_quest
     puts "\n"
-    puts "This is the Granger Quest!"
+    puts "Herminone is trapped in Malfoy Manor and needs to escape to save Ron and Harry."
   end
 end
