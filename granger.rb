d3a5abb6601506c7d117b1eba077033a9021d8cc
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-28 12:30:18 +0000

    added spacing for readability

--- granger.rb
+++ granger.rb
@@ -3,6 +3,7 @@ class Granger
     puts "\n"
     puts "Herminone is trapped in Malfoy Manor and needs to escape to save Ron and Harry."
     puts "Bellatrix tied her up, but she still has access to her wand."
+    
     user_choice = gets.chomp.to_i
   end
 end
