a611b92a55141b21afce0554565d5cfd94c19c3d
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-28 13:54:33 +0000

    added puts statements

--- granger.rb
+++ granger.rb
@@ -4,6 +4,10 @@ class Granger
     puts "Herminone is trapped in Malfoy Manor and needs to escape to save Ron and Harry."
     puts "Bellatrix tied her up, but she still has access to her wand. What should she do?"
 
+    puts "1. Use the wand"
+    puts "2. Try to untie the rope"
+    puts "3. Quit"  
+
     user_choice = gets.chomp.to_i
 
     case user_choice
